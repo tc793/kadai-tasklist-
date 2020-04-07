44084be5c7dc2089c755469b32a3945ca0beffac
tc <ft77725@gmail.com>
2020-04-07 12:00:23 +0000

    CRUD messages4

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -17,4 +17,8 @@
     </table>
     
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
+    
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
 @endsection
